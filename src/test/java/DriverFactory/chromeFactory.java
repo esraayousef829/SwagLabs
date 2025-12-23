693ae12a54358cdaffbe3581d64f759afa75e258
esraayousef829 <esraayousef829@gmail.com>
2025-12-23 10:45:52 +0200

    commit chrome Factory

---
+++ src/test/java/DriverFactory/chromeFactory.java
@@ -0,0 +1,22 @@
+package DriverFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class chromeFactory {
+    public static WebDriver getChromeDriver(){
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
+
